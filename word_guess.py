b243b800b47337450f7bce106affee7004420e76
AVNEETK99 <57584933+AVNEETK99@users.noreply.github.com>
2023-03-15 16:02:05 +0530

    Update word_guess.py

--- word_guess.py
+++ word_guess.py
@@ -1,10 +1,6 @@
 import random
-# library that we use in order to choose
-# on random words from a list of words
 
 name = input("What is your name? ")
-
-# Here the user is asked to enter the name first
 
 print("Good Luck ! ", name)
 
@@ -12,8 +8,6 @@ words = ['rainbow', 'computer', 'science', 'programming',
 		'python', 'mathematics', 'player', 'condition',
 		'reverse', 'water', 'board', 'geeks']
 
-# Function will choose one random
-# word from this list of words
 word = random.choice(words)
 
 
@@ -21,7 +15,6 @@ print("Guess the characters")
 
 guesses = ''
 
-# any number of turns can be used here
 turns = 12
 
 
@@ -55,25 +48,18 @@ while turns > 0:
 		print("The word is: ", word)
 		break
 
-	# if user has input the wrong alphabet then
-	# it will ask user to enter another alphabet
 	print()
 	guess = input("guess a character:")
 
-	# every input character will be stored in guesses
 	guesses += guess
 
-	# check input with the character in word
 	if guess not in word:
 
 		turns -= 1
 
-		# if the character doesn’t match the word
-		# then “Wrong” will be given as output
+
 		print("Wrong")
 
-		# this will print the number of
-		# turns left for the user
 		print("You have", + turns, 'more guesses')
 
 		if turns == 0:
