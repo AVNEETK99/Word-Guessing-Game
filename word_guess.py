4e0973b379ea8b38871e955d6e52e67345c6e5cf
AVNEETK99 <57584933+AVNEETK99@users.noreply.github.com>
2023-03-15 16:02:50 +0530

    Update word_guess.py

--- word_guess.py
+++ word_guess.py
@@ -20,31 +20,25 @@ turns = 12
 
 while turns > 0:
 
-	# counts the number of times a user fails
+
 	failed = 0
 
-	# all characters from the input
-	# word taking one at a time.
 	for char in word:
 
-		# comparing that character with
-		# the character in guesses
+
 		if char in guesses:
 			print(char, end=" ")
 
 		else:
 			print("_")
 
-			# for every failure 1 will be
-			# incremented in failure
+		
 			failed += 1
 
 	if failed == 0:
-		# user will win the game if failure is 0
-		# and 'You Win' will be given as output
+		
 		print("You Win")
 
-		# this print the correct word
 		print("The word is: ", word)
 		break
 
